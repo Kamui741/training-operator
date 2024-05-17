b97647b5112c5cb00ffc5afbab722d91f9f9a54d
Kamui741 <2215702430@qq.com>
2024-05-17 11:51:21 +0800

    new file:   deploy/Dockerfile 	modified:   deploy/deploy.sh 	new file:   deploy/gencerts.tar 	new file:   deploy/init.yaml 	modified:   deploy/rbac.yaml 	modified:   deploy/deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: ChZheng
  # @Date: 2024-05-14 19:33:19
- # @LastEditTime: 2024-05-15 18:22:15
+ # @LastEditTime: 2024-05-17 11:43:56
  # @LastEditors: ChZheng
  # @Description:
  # @FilePath: /training-operator/deploy/deploy.sh
@@ -9,6 +9,7 @@
 echo "开始部署..."
 kubectl create -f namespace.yaml
 kubectl create -f rbac.yaml
+kubectl create -f init.yaml
 kubectl create -f kubeflow.org_tfjobs.yaml
 kubectl create -f kubeflow.org_mxjobs.yaml
 kubectl create -f kubeflow.org_pytorchjobs.yaml
@@ -16,6 +17,6 @@ kubectl create -f kubeflow.org_xgboostjobs.yaml
 kubectl create -f kubeflow.org_mpijobs.yaml
 kubectl create -f kubeflow.org_paddlejobs.yaml
 kubectl create -f webhook.yaml
-./gencerts.sh -n kubeflow
+# ./gencerts.sh -n kubeflow
 kubectl create -f deployment.yaml
 echo "部署完成。"
